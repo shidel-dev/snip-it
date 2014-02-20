234d590b5a9e605d158ac22ebbf19114ab6706c2
Steven Spiel <stevenspiel@gmail.com>
2014-02-20 10:11:02 -0600

    add language model and migration with accompanying passing test

---
+++ spec/models/language_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Language do
+
+  before do
+    @ruby = Language.new(name: 'Ruby')
+    @java = Language.new(name: 'Java')
+    @c = Language.new(name: 'C')
+  end
+
+  describe "#create" do
+    it "increases database count by 1 when saved" do
+      expect{@ruby.save}.to change{Language.all.size}.by(1)
+    end
+  end
+    
+end
