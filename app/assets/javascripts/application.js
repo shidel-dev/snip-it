788604a5bbb62c04346d2dd4679c71b1023aa2ac
Shawn <joeshidel@gmail.com>
2014-02-19 20:23:13 -0600

    display and hide for popup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,14 @@
     return formatedObj
   }
 
+$(document).ready(function() {
+  $('.cards_data').on('click', function(e) {
+    $('.modelHolder').css('display', 'block');
+    var cardData = $(e.target).data('snips') || $(e.target).parent().data('snips');
+  });
+
+  $('.mask').on('click', function(e) {
+    $('.modelHolder').css('display', 'none');
+  });
+});
+
