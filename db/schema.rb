b9ee7734cbd31715573f00f4b57cb862763b1064
Steven Spiel <stevenspiel@gmail.com>
2014-02-20 10:23:52 -0600

    create board model with accompanying migration and first passing test

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220160833) do
+ActiveRecord::Schema.define(version: 20140220162124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "languages", force: true do |t|
     t.string   "name"
