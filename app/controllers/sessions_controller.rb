7f05407c1bd99b9b1e7ad17a8117f88d9d6e90a3
stevenspiel <stevenspiel@gmail.com>
2014-02-20 14:06:59 -0600

    added login and signup pages

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.auhenticate(params[:password])
+      session[:id] = user.id
+      render ''
+    else
+      # return login error?
+      render ''
+    end
+  end
+
+  def new
+  end
+end
