7b0117ea2f4a908b549f2a25f2324ce7d322d875
Steven Spiel <stevenspiel@gmail.com>
2014-02-22 23:22:48 -0600

    add routing for individual boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,4 +7,14 @@ class BoardsController < ApplicationController
     @search_title = "Your Boards"
     @snips = []
   end
+
+  def show
+    redirect _to :root unless current_user
+    @user = User.find(session[:id])
+    @languages = Language.all.order('name')
+    @boards = @user.boards
+    @board = Board.find(params[:id])
+    @search_title = "Snipits in #{@board.title}"
+    @snips = @board.instances.map{|instance| instance.snip }
+  end
 end
